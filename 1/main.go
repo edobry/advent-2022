ce1ca679d99fa56cb225d08eee6f6d145df60cff
edobry <edobry@gmail.com>
2022-12-04 01:46:23 -0500

    solve part two

--- 1/main.go
+++ 1/main.go
@@ -5,10 +5,18 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 )
 
 func main() {
+	var input = parseInput()
+
+	partOne(input)
+	partTwo(input)
+}
+
+func parseInput() map[int]int {
 	// open file stream
 	file, err := os.Open("input.txt")
 
@@ -55,6 +63,15 @@ func main() {
 		currentElfSnacks = append(currentElfSnacks, i)
 	}
 
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return elfSnacks
+}
+
+func partOne(elfSnacks map[int]int) {
+
 	var bestPreparedElf int
 	for k, v := range elfSnacks {
 		if v < elfSnacks[bestPreparedElf] {
@@ -66,9 +83,28 @@ func main() {
 
 	// fmt.Println(elfSnacks)
 
-	fmt.Printf("best prepared elf: #%d with %d calories", bestPreparedElf, elfSnacks[bestPreparedElf])
+	fmt.Printf("best prepared elf: #%d with %d calories\n", bestPreparedElf, elfSnacks[bestPreparedElf])
+}
 
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+func partTwo(elfSnacks map[int]int) {
+	elves := make([][]int, len(elfSnacks))
+
+	i := 0
+	for k := range elfSnacks {
+		elves[i] = []int{k, elfSnacks[k]}
+		i++
 	}
+
+	sort.Slice(elves, func(i, j int) bool {
+		return elves[i][1] < elves[j][1]
+	})
+
+	fmt.Println("finding top three best prepared elves...")
+	var total = 0
+	topThree := elves[len(elves)-3:]
+	for i, elf := range topThree {
+		fmt.Printf("#%d: %d\n", 3-i, elf[1])
+		total += elf[1]
+	}
+	fmt.Printf("total calories: %d\n", total)
 }
